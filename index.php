1cddb0b3917b10d11a243ddbc6978ca3d69ebf3d
Decentrpg <62545529+Decentrpg@users.noreply.github.com>
2020-03-23 13:51:00 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from www.CR-MP-rp.ru/ by HTTrack Website Copier/3.x [XR&CO'2013], Tue, 16 May 2017 07:14:58 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="UTF-8">
+    <title>DECENT-RPG - Играй в GTA:SA по сети на нашем сервере</title>
+	<link rel="icon" href="favicon/favicon.ico"; type="image/x-icon" />
+    <link rel="stylesheet" href="css/maksa988.css">
+    <script src="https://use.fontawesome.com/8a3f41131a.js"></script>
+</head>
+<body>
+    <div id="header" class="bh">
+        <div class="navbar">
+            <div class="container">
+                <div class="logo"></div>
+                 <ul class="nav-menu">
+                    <li class="active"><a href="index.html">Главная</a></li>
+                    <li><a href="how_to_play.html">Как начать играть</a></li>
+                    <li><a href="http://CR-MP.ru/">Форум</a></li>
+                </ul>
+                <div class="social-buttons">
+                <a href="https://vk.com/decentrpg"><i class="fa fa-vk"></i></a>
+            </div>
+            </div>
+        </div>
+        <div class="container" style="text-align: center;">
+            <div class="header-text-block">
+                <h1>DECENT-RPG</h1>
+                <hr class="center white">
+                Играй в GTA:SA онлайн на нашем сервер
+                <a href="samp://212.76.129.106:7689">Играть</a>
+            </div>
+        </div>
+    </div>
+    <div id="content">
+        <div class="how_download">
+            <div class="container">
+                <h1>Необходимо скачать</h1>
+                <hr class="center blue">
+                <div class="download-buttons">
+                    <a href="files/gta_sa.rar" class="download-gta-btn">Скачать</a>
+                    <a href="files/sa-mp-0.3.7-R3-1-install.rar" class="download-multiplayer-btn">Скачать</a>
+                    <a href="samp://212.76.129.106:7689" class="download-mod-btn">Подключиться</a>
+                </div>
+                <img src="images/how_download.png" alt="">
+            </div>
+        </div>
+        <div class="ips">
+            <div class="container">
+                <h1>IP нашего сервера</h1>
+                <div class="server">
+                    <h5 class="number">1</h5>
+                    <p>212.76.129.106:7689</p>
+                    <div class="status status-online"><i class="fa fa-check"> Server Online</i></div>
+                </div>
+                <div class="server">
+                    <h5 class="number">2</h5>
+                    <p><script src="http://gtamonitor.ru/web/online-6310.js"></script>
+		</p>
+                    <div class="status status-online"><i class="fa fa-check"> </i></div>
+                </div>
+            </div>
+        </div>
+        <div class="about">
+            <div class="container">
+                <h1>О проекте DECENT-RPG</h1>
+                <hr>
+                <div class="logo"></div>
+                <div class="about-text">НОВЫЙ сервер samp 0.3.7, на котором частые обновления и добрая администрация!
+                    <br>
+                    Пригласи 3-х друзей и получи админку! <br>
+                    <br>
+                    Если возникли вопросы, то задавайте их в личные сообщения группы.<br>
+                    Решать тебе, стать бандитом или встать на сторону защиты гражданских! <br>
+                    Играй вместе с нами на DECENT-RPG!</div>
+            </div>
+        </div>
+        <div class="social-block">
+            <div class="container">
+                <h1>Мы в социальных сетях</h1>
+                <hr>
+                <p>Если ты активный игрок проекта DECENT-RPG и хочешь
+                    узнавать свежие новости первым, тогда вступай в нашу
+                    группу, поддержи проект своей активностью!
+                </p>
+                <b>Подпишись и узнаем как нас много!</b>
+                <a href="https://vk.com/decentrpg"><i class="fa fa-vk"></i> МЫ ВКОНТАКТЕ</a>
+            </div>
+        </div>
+    </div>
+
+</html>
